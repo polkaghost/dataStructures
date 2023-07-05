5420cde3426a89f23a9f31b4a79028f905f21267
geistmeister <74523173+tomiesghost@users.noreply.github.com>
2023-07-04 23:26:25 -0500

    Add files via upload

---
+++ arrayIterate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i;
+	int numArray[] = {1, 2, 3, 4, 5};
+
+	for(i = 0; i < sizeof(numArray)/sizeof(numArray[0]); i++)
+{
+	printf("%i\n", numArray[i]);
+}
+
+	return 0;
+}
+
